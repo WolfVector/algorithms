c4e7f88a5dae829e0a3327bc8776f410ce39d973
WolfVector <angry_programmer@hotmail.com>
2021-11-01 23:51:35 -0600

    24 math game

---
+++ math24.py
@@ -0,0 +1,26 @@
+from itertools import permutations, combinations, combinations_with_replacement
+
+number = ["1", "2", "3", "4"]
+operations = ["+", "-", "*", "/"]
+groups = ['X+X+X+X', 'X+X+(X+X)', 'X+(X+X)+X', '(X+X+X)+X', '(X+X)+X+X', 'X+(X+X+X)', '((X+X)+X)+X', 'X+(X+(X+X))', 'X+((X+X)+X)', '(X+X)+(X+X)', '(X+(X+X))+X']
+seen = set()
+target = 24
+
+perm = permutations(number) #Get all the arragements
+
+print("Operations")
+for val_arr in perm: #iterate over the arragement of numbers
+    for ope_arr in combinations_with_replacement(operations, 3): #iterate over the arragement of operations
+        #Using the same arragement for the numbers, produce different operations
+        for ope_perm in permutations(ope_arr, 3):
+            #formula_key = "".join(ope_perm + val_arr)
+            for pattern in groups:
+                #Replace the '+' with the operations
+                formula = "".join(l+r for l,r in zip([""] + list(ope_arr), pattern.split('+')))
+                    
+                #Replace 'X' with the numbers
+                formula = "".join(l+r for l,r in zip([""] + list(val_arr), formula.split('X')))
+
+                if formula not in seen and eval(formula) == target:
+                    print(formula, '=', eval(formula))
+                    seen.add(formula)
